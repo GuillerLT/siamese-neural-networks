83a40cc0a4e6d39261c039cc1eb448727f309c88
GuillerLT <glt2@alu.ua.es>
2019-05-24 18:10:14 +0200

    Pair siamese added

---
+++ 02_pair/main.py
@@ -0,0 +1,52 @@
+import keras
+import numpy as np
+import datatool
+import pairgen
+import base
+import siamese
+import predtool
+from base import base_mnist, base_fashion_mnist, base_cifar10
+from keras.datasets import mnist, fashion_mnist, cifar10
+from keras.callbacks import ModelCheckpoint
+
+# Parameters
+dataset = cifar10
+base_model = base_cifar10
+emb_d = 512
+loss = siamese.contrastive_loss1
+optimizer = 'adam'
+n_tr = 100  # Number of train samples per class
+pos = 1  # Number of positive pairs
+neg = 4  # Number of negative pairs
+batch = 32  # Number of samples per class in each epoch
+epochs = 200
+# Data
+mat_tr, mat_te = datatool.load(dataset, n_tr)
+gen_tr = pairgen.pairgen(mat_tr, pos, neg, batch)
+gen_te = pairgen.pairgen(mat_te, pos, neg, batch)
+# Model
+base_model = base_model(mat_tr.shape[2:], emb_d)
+siam_model, emb_model = siamese.create(base_model)
+siamese.compile(siam_model, loss, optimizer)
+# Train
+siam_model.fit_generator(gen_tr,
+                         epochs=epochs,
+                         verbose=2)
+x_tr = np.reshape(mat_tr, (-1,) + mat_tr.shape[2:])
+e_tr = emb_model.predict(x_tr)
+y_tr = np.repeat(np.arange(mat_tr.shape[0]), mat_tr.shape[1])
+x_te = np.reshape(mat_te, (-1,) + mat_te.shape[2:])
+e_te = emb_model.predict(x_te)
+y_te = np.repeat(np.arange(mat_te.shape[0]), mat_te.shape[1])
+# Evaluate
+predtool.hist(e_tr, y_tr, e_te, y_te)
+predtool.svr(e_tr, y_tr, e_te, y_te, 'linear')
+predtool.svr(e_tr, y_tr, e_te, y_te, 'poly')
+predtool.svr(e_tr, y_tr, e_te, y_te, 'rbf')
+predtool.knn(e_tr, y_tr, e_te, y_te, 1)
+predtool.knn(e_tr, y_tr, e_te, y_te, 5)
+predtool.knn(e_tr, y_tr, e_te, y_te, 15)
+predtool.knn(e_tr, y_tr, e_te, y_te, 25)
+predtool.rf(e_tr, y_tr, e_te, y_te, 25)
+predtool.rf(e_tr, y_tr, e_te, y_te, 50)
+predtool.rf(e_tr, y_tr, e_te, y_te, 75)
---
+++ 02_pair/pairgen.py
@@ -0,0 +1,60 @@
+import keras
+import numpy as np
+import math
+
+
+class pairgen(keras.utils.Sequence):
+    positive = 0.0
+    negative = 1.0
+
+    def __init__(self, mat_t, pos, neg, batch):
+        self.mat_t = mat_t
+        self.pos = pos
+        self.neg = neg
+        self.batch = batch
+        self.n_k = mat_t.shape[0]
+        self.n_s = mat_t.shape[1]
+        self.shape = mat_t.shape[2:]
+
+    def __len__(self):
+        return math.ceil(self.n_s / self.batch)
+
+    def __getitem__(self, index):
+        first = index * self.batch
+        last = min(self.n_s, first + self.batch)
+        size = (last - first) * self.n_k * (self.pos + self.neg)
+        x = {
+            'i1': np.empty((size,) + self.shape),
+            'i2': np.empty((size,) + self.shape),
+        }
+        y = np.zeros((size, ))
+        i = 0
+        for j1 in range(first, last):
+            for k1 in range(self.n_k):
+                x1 = self.mat_t[k1][j1]
+                for j2 in np.random.choice(self.n_s, self.pos, False):
+                    while j1 == j2:
+                        j2 = np.random.choice(self.n_s)
+                    x2 = self.mat_t[k1][j2]
+                    if np.random.choice([True, False]):
+                        x['i1'][i] = x1
+                        x['i2'][i] = x2
+                    else:
+                        x['i1'][i] = x2
+                        x['i2'][i] = x1
+                    y[i] = pairgen.positive
+                    i += 1
+                for k2 in np.random.choice(self.n_k, self.neg, False):
+                    while k1 == k2:
+                        k2 = np.random.choice(self.n_k)
+                    j2 = np.random.choice(self.n_s)
+                    x2 = self.mat_t[k2][j2]
+                    if np.random.choice([True, False]):
+                        x['i1'][i] = x1
+                        x['i2'][i] = x2
+                    else:
+                        x['i1'][i] = x2
+                        x['i2'][i] = x1
+                    y[i] = pairgen.negative
+                    i += 1
+        return x, y
